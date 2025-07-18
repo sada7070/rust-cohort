83921bc72a3d0526fe72a42edf5e2c9cf64d770b
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-07-18 09:53:28 +0530

    Borsh

--- week30/src/main.rs
+++ week30/src/main.rs
@@ -1,17 +1,48 @@
-// Serde
+// // Serde
 
-use serde::{ Serialize, Deserialize};
+// use serde::{ Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+// #[derive(Serialize, Deserialize, Debug)]
+// struct User {
+//     username: String,
+//     password: String,
+// }
+
+// fn main() {
+//     let s = String::from("{\"username\": \"sada\", \"password\": \"123123\"}");
+
+//     let u: Result<User, serde_json::Error> = serde_json::from_str(&s);
+
+//     println!("{:?}", u.unwrap());
+// }
+
+
+
+
+// Borsh
+
+use borsh::{BorshSerialize, BorshDeserialize};
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
 struct User {
     username: String,
     password: String,
 }
 
 fn main() {
-    let s = String::from("{\"username\": \"sada\", \"password\": \"123123\"}");
+    let user = User {
+        username: String::from("sada"),
+        password: String::from("sada98767"),
+    };
+    let mut serializing: Vec<u8> = Vec::new();      // empty vector
 
-    let u: Result<User, serde_json::Error> = serde_json::from_str(&s);
+    let ans = user.serialize(&mut serializing);                    // serializing user
 
-    println!("{:?}", u.unwrap());
+    match ans {
+        Ok(_) => println!("{:?}", serializing),
+        Err(_) => println!("Error while serialization."),
+    }
+
+    let deserializing = User::try_from_slice(&serializing).unwrap();                   // deserializing user
+    println!("{:?}", deserializing);
 }
