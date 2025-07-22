ffb118b42448b898eb56e133bb341228ba4fa4ff
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-07-22 07:58:22 +0530

    lifetimes

--- week30/src/main.rs
+++ week30/src/main.rs
@@ -19,30 +19,57 @@
 
 
 
-// Borsh
+// // Borsh
 
-use borsh::{BorshSerialize, BorshDeserialize};
+// use borsh::{BorshSerialize, BorshDeserialize};
 
-#[derive(BorshSerialize, BorshDeserialize, Debug)]
-struct User {
-    username: String,
-    password: String,
-}
+// #[derive(BorshSerialize, BorshDeserialize, Debug)]
+// struct User {
+//     username: String,
+//     password: String,
+// }
+
+// fn main() {
+//     let user = User {
+//         username: String::from("sada"),
+//         password: String::from("sada98767"),
+//     };
+//     let mut serializing: Vec<u8> = Vec::new();      // empty vector
+
+//     let ans = user.serialize(&mut serializing);                    // serializing user
+
+//     match ans {
+//         Ok(_) => println!("{:?}", serializing),
+//         Err(_) => println!("Error while serialization."),
+//     }
+
+//     let deserializing = User::try_from_slice(&serializing).unwrap();                   // deserializing user
+//     println!("{:?}", deserializing);
+// }
+
+
+
+
+
+// Lifetimes
 
 fn main() {
-    let user = User {
-        username: String::from("sada"),
-        password: String::from("sada98767"),
-    };
-    let mut serializing: Vec<u8> = Vec::new();      // empty vector
-
-    let ans = user.serialize(&mut serializing);                    // serializing user
-
-    match ans {
-        Ok(_) => println!("{:?}", serializing),
-        Err(_) => println!("Error while serialization."),
+    let str1= String::from("sada");
+    let ans;
+    {
+        let str2 = String::from("shiva");
+        ans = longest_str(&str1, &str2);
+        println!("{}", ans);
+        
     }
 
-    let deserializing = User::try_from_slice(&serializing).unwrap();                   // deserializing user
-    println!("{:?}", deserializing);
+    //println!("{}", ans);
+}
+
+fn longest_str<'a>(s1: &'a String, s2: &'a String) -> &'a String {
+    if s1.len() > s2.len() {
+        return  s1;
+    } else {
+        return s2;
+    }
 }
